fae1ad72112cfe8b2509fe6c48460b4f13c9f3e6
Hochul Seok <ultrafellen@gmail.com>
2025-04-04 14:39:55 +0900

    add build and deploy environments

---
+++ scripts/HPP_L1_Token.deploy.js
@@ -0,0 +1,27 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  console.log("Deploying HousePartyProtocol Token contract...");
+
+const HPP_TOKEN_Recipient = "0x1975552499c759bEAE09Fd93a57F45B7D4C84B54"; // AERGO Token Address
+
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying with account:", deployer.address);
+
+  const HPPToken = await ethers.getContractFactory("HousePartyProtocol");
+  const token = await HPPToken.deploy(
+    HPP_TOKEN_Recipient,
+    deployer.address // initialOwner
+  );
+
+  await token.waitForDeployment();
+  console.log("HousePartyProtocol deployed to:", await token.getAddress());
+  console.log("HousePartyProtocol token recipient to:", HPP_TOKEN_Recipient);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
